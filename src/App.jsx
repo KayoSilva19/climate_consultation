e9959cbf173b982f2095df0408fc2c69e48ac522
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-18 16:23:44 -0300

    API connect

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,33 @@ import { Previsao } from './components/Previsao'
 import { Wrapper } from './components/Wrapper'
 
 function App() {
-  const [cidade, setCidade] = useState('')
+  const [cidade, setCidade] = useState(
+    localStorage.getItem('cidade_atual') || '',
+  )
+  const [clima, setClima] = useState(null)
+  const [previsao, setPrevisao] = useState([])
+
+  const apiKey = import.meta.env.VITE_API_KEY || ''
+
+  async function buscarClima() {
+    try {
+      const responseClima = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`,
+      )
+
+      setClima(responseClima.data)
+      localStorage.setItem('cidade_atual', cidade)
+    } catch (error) {
+      console.log('Error:', error)
+    }
+  }
 
   return (
     <Wrapper>
       <h1>Condições Climáticas</h1>
-      <Busca />
-      <ClimaAtual />
-      <Previsao />
+      <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
+      {clima && <ClimaAtual clima={clima} />}
+      {previsao.length > 0 && <Previsao clima={clima} />}
     </Wrapper>
   )
 }
--- src/components/Busca/index.jsx
+++ src/components/Busca/index.jsx
@@ -1,8 +1,13 @@
-export function Busca() {
+export function Busca({ cidade, setCidade, buscarClima }) {
   return (
     <div>
-      <input type="text" placeholder="Digite o nome da cidade" />
-      <button>Buscar</button>
+      <input
+        type="text"
+        value={cidade}
+        onChange={(e) => setCidade(e.target.value)}
+        placeholder="Digite o nome da cidade"
+      />
+      <button onClick={buscarClima}>Buscar</button>
     </div>
   )
 }
